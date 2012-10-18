ce6da17fdfe6f35e30d1f3f2a39041af77323734
Daniel Schudel <dan.schudel@gmail.com>
2012-10-18 18:49:02 -0400

    adding the general structure

---
+++ New Mindmap.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1350600422215" ID="ID_1889660484" MODIFIED="1350600422215" TEXT="New Mindmap"/>
+</map>
